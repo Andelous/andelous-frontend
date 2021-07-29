727cd732ea28360fdf6cc1c6379a7a08bc46b5c7
Angel Daniel Alonso Tovar <andelous.aa@gmail.com>
2021-07-29 18:49:52 -0500

    First draft

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <v-row cols>
+            <v-col cols="12" sm="2" xl="3"></v-col>
+            <v-col cols="12" class="text-center" sm="4" xl="3">
+                My Image
+            </v-col>
+            <v-col cols="12" sm="4" xl="3">
+                <h2 class="mb-4">Who am I&quest;</h2>
+                <p>
+                    My name is Angel, but you can call me by my pseudonym,
+                    <strong>Andelous</strong>. I built this page just for the
+                    sake of fun. I hope you like what you see.
+                </p>
+                <p>
+                    If you're a recruiter, you can see here some of the skills I
+                    have, and you can decide whether I may be a good fit for the
+                    position you're recruiting <code>;)</code>.
+                </p>
+            </v-col>
+            <v-col cols="12" sm="2" xl="3"></v-col>
+        </v-row>
+
+        <h2>Technologies I use</h2>
+
+        <v-row></v-row>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    name: "Home",
+
+    data: () => ({}),
+};
 </script>
